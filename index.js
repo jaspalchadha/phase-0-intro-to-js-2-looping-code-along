fcad8e454466c59ab0c45987f9f7b1854bf191e1
jaspalsinghchadha <jaspalchadha2@gmail.com>
2023-03-12 14:16:29 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+let frein = ["john", "candice", "andy"]
+let cards = []
+function writeCards(frein, eventNames){
+    for ( let num = 0; num < frein.length; num++ ) {
+        cards.push(`Thank you, ${frein[num]}, for the wonderful ${eventNames} gift!`);
+    
+    } 
+  return cards;
+  
+}
+
+function countDown(num){
+    while(num > -1){
+        console.log (num);
+        num--;
+    }
+}
